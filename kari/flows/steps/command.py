62a60d46da43e7c994fcbb16fa083c03bef248f0
Pedro Melendez <pmelendez@pevicom.com>
2024-06-06 08:01:52 -0400

    Fixing unit test

--- kari/flows/steps/command.py
+++ kari/flows/steps/command.py
@@ -1,7 +1,7 @@
 
 def make_command_step(config):
     def step(input, state):
-        output = eval(config.command)
+        output = eval(config['command'])
         state[f'step_{config["name"]}_output'] = output
         return output
     return step
