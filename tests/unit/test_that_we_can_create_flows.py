332140ef29a13eb38b4a422c9e7b28fcda187060
Pedro Melendez <pmelendez@pevicom.com>
2024-07-02 19:28:08 -0400

    Fixing unit test

--- tests/unit/test_that_we_can_create_flows.py
+++ tests/unit/test_that_we_can_create_flows.py
@@ -1,10 +1,10 @@
 from kari.flows.config import load_config
-from kari.flows.flow import make_flow
+from kari.flows.flow import make_flows
 config_path = "tests/files/generic_flow.yml"
 
 def test_that_we_read_flows_config_files():
-    config = load_config(config_path)
-
+    config = load_config(config_path)['flows'][0]
+    
     assert config['type'] == 'linear'
     assert len(config['steps']) == 4
     assert config['steps'][0]['type'] == 'command'
@@ -13,8 +13,8 @@ def test_that_we_read_flows_config_files():
 
 def test_that_we_create_a_flow():
     config = load_config(config_path)
-    flow = make_flow(config)
-    print(flow)
+    flow = make_flows(config)
+
     # Assert that flow is a function
-    assert callable(flow)
-    output = flow({})
+    assert callable(flow[0])
+    output = flow[0]({})
